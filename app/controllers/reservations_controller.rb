b75a51613f83d478df3ad749dc8a17b7dd4cf1c1
TTwithTT <puttyo76@gmail.com>
2023-02-16 01:22:45 +0900

    [add] confirm & create to reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,19 +3,27 @@ class ReservationsController < ApplicationController
 		@reservations = Reservation.all
 	end
 
-	def new
-		@reservations = Reservation.new
-	end
-
 	def confirm
 		@reservations = Reservation.new(reservation_params)
-		if @reservation.invalid?
+	end
+	
+	def create
+		@reservation = Reservation.new(reservation_params)
+		if params[:back]
 			redirect_back(fallback_location: root_path) 
 		end
+
+		respond_to do |format|
+			if @reservation.save
+				redirect_to :reservations
+			else
+				redirect_to :rooms
+			end
 	end
 
+	private
 	# def reservation_params
 	# 	params.require(:reservation).permit(:etc).merge(user_id: current_user.id)
 	# end
-	
+
 end
