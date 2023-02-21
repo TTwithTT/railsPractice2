8d6f7dcbe4c33e12c7912054699e04acba836003
TTwithTT <puttyo76@gmail.com>
2023-02-21 18:49:46 +0900

    [add] validation on reservation and room

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,9 @@ class Room < ApplicationRecord
 	has_many :reservations
 
   mount_uploader :avatar, AvatarUploader
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :price, presence: true
+	validates :address, presence: true
 end
